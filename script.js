07ba695995f643ac140988aa456f83381526ca56
Kim Duclos <kimduclos@gmail.com>
2018-02-11 10:10:41 -0700

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var searchQuery, title, snippet;
+/* var searchQuery, title, snippet;
 
 function search(){
     $(".searchBtn").on('keyup', function (e) {
@@ -8,4 +8,23 @@ function search(){
                 document.getElementById("snippet").innerHTML = search[0].snippet;    
             });
         });
-    };
+    }; */
+
+$(document).ready(function() {
+    document.getElementById("textInput").hidden=true;
+});    
+
+function toggleHide() {
+    document.getElementById("icon").hidden=true;
+    document.getElementById("textInput").hidden=false;
+    document.getElementById("searchTxt").hidden=true;
+}
+
+function handle(e) {
+    if(e.keycode === 13) {
+        $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchQuery, function(json) {
+                document.getElementById("title").innerHTML = search[0].title;
+                document.getElementById("snippet").innerHTML = search[0].snippet; 
+        });
+    }
+}
