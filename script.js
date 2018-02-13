e29cbd1156a4cf24bde5c1736b36b974d16b3928
Kim Duclos <kimduclos@gmail.com>
2018-02-13 08:17:42 -0700

    update

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-/* var searchQuery, title, snippet;
+var searchQuery;
 
-function search(){
-    $(".searchBtn").on('keyup', function (e) {
-        searchQuery = document.getElementById('textInput').value;
-            $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchQuery, function(json) {
-                document.getElementById("title").innerHTML = search[0].title;
-                document.getElementById("snippet").innerHTML = search[0].snippet;    
-            });
-        });
-    }; */
+/*function search(){
+    
+};*/
 
 $(document).ready(function() {
     document.getElementById("textInput").hidden=true;
@@ -16,7 +10,17 @@ $(document).ready(function() {
     $('#search').keydown(function(e) {
         if (e.keyCode == 13) {
             $('#search').submit();
-            alert("working");
+            searchQuery = document.getElementById('textInput').value;
+            $.getJSON("https://en.wikipedia.org/w/api.php?callback=?",
+            {
+                "action":"query",
+                "format":"json",
+                "srsearch":searchQuery,
+                "list":"search"
+            }).done(function(data) {
+                document.getElementById("title").innerHTML= JSON.stringify(data);
+                document.getElementById("snippet").innerHTML=search[0].title;
+            });
         }
     });
 });    
