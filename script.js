50d90a7ab8e4f15da2282f51bac2e98a12dbe45f
Kim Duclos <kimduclos@gmail.com>
2018-03-25 11:59:34 -0600

    Update

--- script.js
+++ script.js
@@ -5,26 +5,24 @@ $(document).ready(function() {
     // set text box to hidden until search icon is clicked
     document.getElementById("textInput").hidden=true;
     // enable search with enter key press
-    $('#search').keydown(function(e) {
-        if (e.keyCode == 13) {
-            $('#search').submit();
-            // fill space below search with JSON output from Wiki topic
-            searchQuery = document.getElementById('textInput').value;
-            $.getJSON("https://en.wikipedia.org/w/api.php?callback=?",
-            {
-                "action":"query",
-                "format":"json",
-                "srsearch":searchQuery,
-                "list":"search"
-            }).done(function(data) {
-                // format JSON output
-                document.getElementById("title").value= JSON.stringify(data);
-                document.getElementById("snippet").value=search[0].snippet;
-                document.getElementById("snippet").value=data[3][1];
-            });
-        }
+    $('#search').submit(function(e) {
+        e.preventDefault()
+    
+        searchQuery = document.getElementById('textInput').value;
+        $.getJSON("https://en.wikipedia.org/w/api.php?callback=?",
+        {
+          "action":"query",
+          "format":"json",
+          "srsearch":searchQuery,
+          "list":"search"
+        }).done(function(data) {
+          console.log(JSON.stringify(data))
+            document.getElementById("title").value= JSON.stringify(data);
+            document.getElementById("snippet").value=search[0].snippet;
+            document.getElementById("snippet").value=data[3][1];
+        })
+});
     });
-});    
 // initially set search icon and search box to hidden, with
 function toggleHide() {
     document.getElementById("icon").hidden=true;
